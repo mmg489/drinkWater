80d4189055e64685d453c601a9f9b040efe20737
Magi Munoz <m_mg489@outlook.com>
2023-08-14 13:18:19 -0400

    removed leftover html test element and adding variables in script js

---
+++ script.js
@@ -0,0 +1,9 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+
+smallCups.forEach((cup,idx) => {
+    console.log(idx)
+})
